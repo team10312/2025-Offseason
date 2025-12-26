180fde61f65487e78193d17428d9786c8d1cffe5
team10312 <kumar.sidharthram@gmail.com>
2025-12-26 16:54:23 -0600

    onboard led fix

--- src/main/java/frc/robot/subsystems/Leds.java
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -6,9 +6,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.led.*;
-import com.ctre.phoenix.led.CANdle.LEDStripType;
-import com.ctre.phoenix.led.CANdle.VBatOutputMode;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.Enable5VRailValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
+import com.ctre.phoenix6.signals.VBatOutputModeValue;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.SolidColor;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -22,26 +27,30 @@ public class Leds extends SubsystemBase {
         return mInstance;
     }
 
-    private final CANdle m_leds;
+    private final CANdle m_candle;
     private final int ledCount = 20;
 
     public Leds() {
 
-    m_leds = new CANdle(44, "rio");
-
-    CANdleConfiguration configAll = new CANdleConfiguration();
-    configAll.statusLedOffWhenActive = true;
-    configAll.disableWhenLOS = false;
-    configAll.stripType = LEDStripType.GRB;
-    configAll.brightnessScalar = 0.1;
-    configAll.vBatOutputMode = VBatOutputMode.Modulated;
-    m_leds.configAllSettings(configAll, 100);
+        m_candle = new CANdle(44, "rio");
+        var cfg = new CANdleConfiguration();
+        /* set the LED strip type and brightness */
+        cfg.LED.StripType = StripTypeValue.GRB;
+        cfg.LED.BrightnessScalar = 0.5;
+        cfg.CANdleFeatures.VBatOutputMode = VBatOutputModeValue.Off;
+        cfg.CANdleFeatures.Enable5VRail = Enable5VRailValue.Enabled;
+        /* disable status LED when being controlled */
+        cfg.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Disabled;
+        m_candle.getConfigurator().apply(cfg);
   }
 
   public void setColor(int r, int g, int b){
-    m_leds.setLEDs(r, g, b, 0, 0, ledCount);
+    m_candle.setControl(new SolidColor(0, ledCount).withColor(new RGBWColor(r, g, b)));
   }
 
+
+
+  
   @Override
   public void periodic() {
 
